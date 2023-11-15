9879391cfbae47cc54a4a899b6e03e83d14cb8ab
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-15 17:48:34 +0900

    테스트코드 실습

---
+++ src/main/java/org/example/RandomPasswordGenerator.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import org.passay.*;
+
+public class RandomPasswordGenerator implements org.example.PasswordGenerator{
+
+    public static final String ALLOWED_SPL_CHARACTERS = "!@#$%^&*()_+";
+
+    public static final String ERROR_CODE = "ERRONEOUS_SPECIAL_CHARS";
+
+    public String generatePassword(){
+        org.passay.PasswordGenerator gen = new org.passay.PasswordGenerator();
+
+        CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
+        CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);
+        lowerCaseRule.setNumberOfCharacters(2);
+
+        CharacterData upperCaseChars = EnglishCharacterData.UpperCase;
+        CharacterRule upperCaseRule = new CharacterRule(upperCaseChars);
+        upperCaseRule.setNumberOfCharacters(2);
+
+        CharacterData digitChars = EnglishCharacterData.Digit;
+        CharacterRule digitRule = new CharacterRule(digitChars);
+        upperCaseRule.setNumberOfCharacters(2);
+
+        CharacterData specialChars = new CharacterData() {
+            @Override
+            public String getErrorCode() {
+                return ERROR_CODE;
+            }
+
+            @Override
+            public String getCharacters() {
+                return ALLOWED_SPL_CHARACTERS;
+            }
+        };
+        CharacterRule sqlCharRule = new CharacterRule(specialChars);
+        sqlCharRule.setNumberOfCharacters(2);
+
+        // 0 ~ 12
+        return gen.generatePassword((int) (Math.random() * 13), sqlCharRule, lowerCaseRule, upperCaseRule, digitRule);
+
+    }
+}
--- src/test/java/org/example/PasswordValidatorText.java
+++ src/test/java/org/example/PasswordValidatorText.java
@@ -15,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class PasswordValidatorText {
 
+
     @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
     @Test
     void validatePasswordTest() {
