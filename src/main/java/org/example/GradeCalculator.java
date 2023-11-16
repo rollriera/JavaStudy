46bd4038a781bed04e12e9cfa2a948c771223aae
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-16 18:03:53 +0900

    평균학점 계산기 테스트 코드 작성

---
+++ src/main/java/org/example/GradeCalculator.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.List;
+
+public class GradeCalculator {
+
+    private final Courses courses;
+
+    public GradeCalculator(List<Course> courses) {
+        this.courses = new Courses(courses);
+    }
+
+    /**
+     * 요구사항
+     * 평균학점 계산 방법 = (학점수*교과목 평점)의 합계/수강신청 총학점 수
+     * 일급 컬렉션 사용
+     */
+
+    public double calculateGrade() {
+        // (학점수 * 교과목 평점)의 합계
+        double totalMultipliedCreaditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
+
+
+        //수강신청 총학점 수
+        int totalCompletedCredit = courses.calculateTotalComplatedCredit();
+
+        return totalMultipliedCreaditAndCourseGrade / totalCompletedCredit;
+    }
+}
