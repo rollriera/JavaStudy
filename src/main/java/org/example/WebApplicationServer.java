2a7be8fea0f5f0376d826f1a7fc4ef88b8334d2b
LAPTOP-MOC2AL98\82104 <kimsej45@naver.com>
2023-11-19 20:36:35 +0900

    테스트코드

--- src/main/java/org/example/WebApplicationServer.java
+++ src/main/java/org/example/WebApplicationServer.java
@@ -16,6 +16,7 @@ public class WebApplicationServer {
         tomcat.setPort(8080);
 
         tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+
         log.info("configuring app with basedir: {}", new File("/" + webappDirLocation).getAbsolutePath());
 
         tomcat.start();
